95abb37289a34d0c2574c5f35c620c5f2ef6506f
135777Derick <117374653+135777Derick@users.noreply.github.com>
2022-12-11 08:33:38 +0300

    Create TempConver.cpp

---
+++ TempConver.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() { 
+double fahrenheit,celsius; 
+cout<<"Enter in farenheit : "; 
+cin>>fahrenheit; 
+celsius = 5/9 * fahrenheit ; 
+cout<<"The value of temperature in celcius is :"<<celsius<<endl; 
+return 0; 
+}
